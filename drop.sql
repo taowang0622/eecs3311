2f9c86947aba8c249824a1b5dfad0aedf112523b
TaoWang <taowang0622@gmail.com>
2018-04-07 22:03:24 -0400

    add two SQL files

---
+++ drop.sql
@@ -0,0 +1,18 @@
+-- ===========================================================================
+--       title: Raccoon Rhapsody MMORPG DB (drop)
+-- description: schema destroy script
+--      author: Parke Godfrey
+--    creation: November 2016
+
+-- -------------------------------------------------------------------
+-- Schema definition
+
+DROP TABLE Loot;
+DROP TABLE Treasure;
+DROP TABLE Actor;
+DROP TABLE Quest;
+DROP TABLE Visit;
+DROP TABLE Realm;
+DROP TABLE Avatar;
+DROP TABLE Race;
+DROP TABLE Player;
